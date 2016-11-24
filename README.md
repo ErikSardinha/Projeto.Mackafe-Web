279d6b9a119626e06135c9dcafa5d6eb6a57960a
Bernard Wahba <benny.bw@hotmail.com>
2016-11-24 00:59:18 +0000

    Correções

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # MackCafe
 ## Autores: Bernard Wahba. Erik Santos Sardinha.
 
-
-
-1. 
-2. Descrição: Site para fanaticos por café onde temos todas as informações 
+ 
+1. Descrição: Site para fanaticos por café onde temos todas as informações 
 necessárias para ficar por dentro dos melhores lugares do mack.
--- script.js
+++ script.js
@@ -25,3 +25,18 @@ function FecharMenu(){
 document.getElementsByTagName("nav")[0].style.display = "none";
 document.getElementById("FechaMenu")[0].style.display = "none";
 }
+var ul;
+var liItems; 
+var imageWidth;
+var imageNumber;
+
+function init(){
+
+    ul = document.getElementById(‘image_slider’);
+    liItems = ul.children;
+    imageNumber = liItems.length;
+    imageWidth = liItems[0].children[0].offsetWidth;
+    // set ul’s width as the total width of all images in image slider.
+    ul.style.width = parseInt(imageWidth * imageNumber) + ‘px’;
+    slider(ul);
+}
